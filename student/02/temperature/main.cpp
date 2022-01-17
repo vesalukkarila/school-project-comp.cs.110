a7f6166b45a5f402195c77914a3447ce44d4788b
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-01-17 14:46:12 +0200

    Tehtava yhdessa tiedostossa

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main()
 {
     double lampotila = 0;
-    cout << "Enter a temperature: "<< endl;
+    cout << "Enter a temperature: ";
     cin >> lampotila;
+    double fahrenheit = (lampotila * 1.8) +32;
     double celsius = (lampotila - 32) / 1.8;
+    cout << lampotila <<" degrees Celsius is " << fahrenheit <<" degrees Fahrenheit" << endl;
     cout << lampotila <<" degrees Fahrenheit is " << celsius <<" degrees Celsius" << endl;
 
 
