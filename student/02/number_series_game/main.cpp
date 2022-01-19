e208a499bd7c2933f216340181a0d20c84d26e88
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-01-19 09:50:12 +0200

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
+    int montako = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> montako;
+
+    for (int luku = 1; luku <=montako; ++luku)
+    {
+        std::cout << luku << std::endl;
+    }
+
 
     return 0;
 }
