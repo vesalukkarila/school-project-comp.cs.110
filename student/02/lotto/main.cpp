2ed97649366ad2642a5a42247a51e85ed21c2f29
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-01-20 10:46:34 +0200

    Totaalipallot ja nostetut kyselyt ja sijoitukset

---
+++ student/02/lotto/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long int totaalimr = 0;
+    unsigned long int nostetut = 0;
+    cout << "Enter the total number of lottery balls: ";
+    cin >> totaalimr;
+    cout << "Enter the number of drawn balls: ";
+    cin >> nostetut;
+
+    return 0;
+}
