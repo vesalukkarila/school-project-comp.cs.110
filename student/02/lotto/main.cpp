862f1d61c35ff51b8b132a096a9fe3de70082573
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-01-20 11:19:21 +0200

    Funktiot valmiina tulostukset puuttuu

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+unsigned long int kertomafunktio (unsigned long int p)
+{
+    unsigned long int kertoja = 1;
+    unsigned long int tulo = 1;
+    while (kertoja <= p )
+    {
+        tulo *= kertoja;
+        ++kertoja;
+    }
+
+    return tulo;
+}
+
+unsigned long int laskenta(unsigned long int t, unsigned long int n)
+{
+    unsigned long int totaalinkertoma = kertomafunktio (t);
+    unsigned long int os_sulkeet = kertomafunktio (t-n);
+    unsigned long int nostettujenkertoma = kertomafunktio (n);
+    return totaalinkertoma/(os_sulkeet * nostettujenkertoma);
+
+}
+
 int main()
 {
     unsigned long int totaalimr = 0;
@@ -11,5 +33,15 @@ int main()
     cout << "Enter the number of drawn balls: ";
     cin >> nostetut;
 
+    if (totaalimr >0 and nostetut > 0)
+    {
+        unsigned long int tulos = laskenta (totaalimr, nostetut);
+
+    }
+
+
     return 0;
 }
+
+
+//mitkä kaikki pitää olla unsigned long int????
