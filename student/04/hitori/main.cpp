dc298142af09c171bf42ace0790c99822ff7242b
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-02-09 16:48:45 +0200

    Valmis ja toimii

--- student/04/hitori/main.cpp
+++ student/04/hitori/main.cpp
@@ -1,8 +1,43 @@
+ /* Hitori
+ *
+ * Kuvaus:
+ *   Ohjelma toteuttaa Hitori-pelin. Pelissä on peliruudukko kooltaan
+ * 5 x 5. Kukin ruutu sisältää jonkin numeroista 1-5. Vaaka- ja
+ * pystyriveillä voi aluksi olla useita samoja numeroita, mutta
+ * tarkoituksena on poistaa numeroita niin, että kullakin vaaka- ja
+ * pystyrivillä on kutakin numeroa vain yksi tai ei yhtään. Kuitenkaan
+ * vierekkäisten ruutujen numeroita ei saa poistaa, eikä mikään jäljelle
+ * jäävä numero (ruutu) saa jäädä irralleen muista, eli sen ympäriltä
+ * (yläpuolelta, alapuolelta, vasemmalta, oikealta) ei saa poistaa
+ * kaikkia numeroita.
+ *   Aluksi käyttäjältä kysytään, täytetäänkö peliruudukko satunnaisesti
+ * arvottavilla luvuilla 1-5 vai käyttäjän valitsemilla 25 luvulla.
+ * Ensimmäisessä vaihtoehdossa käyttäjältä kysytään satunnaisluku-
+ * generaattorin siemenlukua ja jälkimmäisessä häntä pyydetään syöttämään
+ * 25 lukua.
+ *   Joka kierroksella käyttäjältä kysytään poistettavan numeron
+ * koordinaatteja eli kahta lukua. Peli päättyy pelaajan voittoon,
+ * jos peliruudukon kaikilla vaaka- ja pystyriveillä esiintyy kukin
+ * numero 1-5 korkeintaan kerran. Peli päättyy pelaajan häviöön,
+ * jos hän poistaa jo poistetun numeron viereisen numeron tai jos
+ * jokin numero jää irralleen muista.
+ *   Ohjelma tarkistaa annetut koordinaatit. Koordinaattien pitää olla
+ * peliruudukon sisällä, ja niiden osoittaman ruudun pitää sisältää
+ * numero, eli jo poistettua numeroa ei voi poistaa toiseen kertaan.
+ *   Pelin päättyessä kerrotaan, voittiko vai hävisikö pelaaja.
+ *
+ * Ohjelman kirjoittaja
+ * Nimi: Vesa Lukkarila
+ * Opiskelijanumero: 150372523
+ * Käyttäjätunnus: h6vlukka
+ * E-Mail: vesa.lukkarila@tuni.fi
+*/
+
 #include <iostream>
 #include <vector>
 #include <cctype>
 #include <random>
-#include <string>
+#include <algorithm>
 using namespace std;
 
 const unsigned int BOARD_SIDE = 5;
@@ -11,13 +46,10 @@ const unsigned char EMPTY = ' ';
 // Muuttaa annetun numeerisen merkkijonon vastaavaksi kokonaisluvuksi
 // (kutsumalla stoi-funktiota).
 // Jos annettu merkkijono ei ole numeerinen, palauttaa nollan.
-//
-// Converts the given numeric string to the corresponding integer
-// (by calling stoi).
-// If the given string is not numeric, returns zero.
-unsigned int stoi_with_check(const string& str) // joka muuttaa annetun numeerisen merkkijonon vastaavaksi kokonaisluvuksi
-{                                               //MITÄ TOI CONST TOS EDESSÄ TEKIKÄÄN? funktio ei tee muutoksia parametriin, &on viiteparametri
-    bool is_numeric = true;                     //eli viittaa välitettyyn parametriin joten voisi muuttaa mutta const estää sen, isot tiedostot..
+// Paluuarvona luonnollinen luku, viiteparametrina merkkijono.
+unsigned int stoi_with_check(const string& str)
+{
+    bool is_numeric = true;
     for(unsigned int i = 0; i < str.length(); ++i)
     {
         if(not isdigit(str.at(i)))
@@ -37,8 +69,9 @@ unsigned int stoi_with_check(const string& str) // joka muuttaa annetun numeeris
 }
 
 // Tulostaa pelilaudan rivi- ja sarakenumeroineen.
-//
-// Prints the game board with row and column numbers.
+// Viiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
+// Ei paluuarvoa.
 void print(const vector<vector<int>>& gameboard)
 
 {
@@ -64,8 +97,11 @@ void print(const vector<vector<int>>& gameboard)
     cout << "=================" << endl;
 }
 
+// Arpoo random kirjaston metodia hyödyntäen 25 lukua, jotka tallentaa
+// vector-tyyppisen pelilaudan 5:een vektorin 5:een alkioon
+// Ei paluuarvoa, viiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
 void arvo_pelilaudan_numerot(vector<vector<int>>& pelilauta)
-
 {
     int siemen;
     cout<< "Enter a seed value: ";
@@ -82,11 +118,14 @@ void arvo_pelilaudan_numerot(vector<vector<int>>& pelilauta)
             rivi.push_back(luku);
         }
         pelilauta.push_back(rivi);
-
     }
 }
 
-void kysy_pelilaudan_numerot(vector<vector<int>>& pelilauta)  //patametrina ja paluuarvona pelilauta viiteparametrina johon tehdään muutoksia
+// Kysyy käyttäjältä 25 lukua, jotka tallentaa
+// vector-tyyppisen pelilaudan 5:een vektorin 5:een alkioon
+// Ei paluuarvoa, viiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
+void kysy_pelilaudan_numerot(vector<vector<int>>& pelilauta)
 {
     int syote;
     cout << "Input: ";
@@ -100,9 +139,10 @@ void kysy_pelilaudan_numerot(vector<vector<int>>& pelilauta)  //patametrina ja p
         }
         pelilauta.push_back(rivi);
     }
-
 }
 
+// Kysyy käyttäjältä kuinka pelilauta esitäytetään.
+// Ei parametria, paluuarvona mainiin string.
 string kysy_tayttotapaa()
 {
     while (true)
@@ -120,15 +160,19 @@ string kysy_tayttotapaa()
             else if (tolower(merkki) == 'i')
                 return "kysy";
         }
-
         else
             continue;
     }
 }
 
+
+// Tarkistaa onko vaaka-tai pystyriveillä vierekkäisiä tyhjiä ruutuja.
+// Paluuarvona false jos häviön kriteeri täyttyy, viiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
 bool tyhjat_vierekkain(vector<vector<int>>& pelilauta)
 {
-    for (int y_laskuri = 0; y_laskuri<5; ++y_laskuri)   //tarkistaa ensin vaakasuuntaiset rivit, onko tyhjiä vierekkäin
+    // Tarkistaa ensin vaakasuuntaiset rivit, onko tyhjiä vierekkäin.
+    for (int y_laskuri = 0; y_laskuri<5; ++y_laskuri)
     {
         for (int x_laskuri = 1; x_laskuri<4; ++x_laskuri)
         {
@@ -138,8 +182,8 @@ bool tyhjat_vierekkain(vector<vector<int>>& pelilauta)
                 return false;
         }
     }
-
-    for (int x_laskuri = 0; x_laskuri<5; ++x_laskuri)   //sitten tarkistetaan pystyrivit
+    // Sitten tarkistetaan pystyrivit, onko tyhjiä vierekkäin.
+    for (int x_laskuri = 0; x_laskuri<5; ++x_laskuri)
     {
         for (int y_laskuri = 1; y_laskuri<4; ++y_laskuri)
         {
@@ -153,42 +197,56 @@ bool tyhjat_vierekkain(vector<vector<int>>& pelilauta)
     return true;
 }
 
-bool jaako_numero_yksin(vector<vector<int>>& pelilauta) //tää on raskas tyyli käydä läpi mutten keksinyt mikä olisi oikea tapa
+// Tarkistaa valitettavan raskaalla tavalla jääkö jokin numero irralleen toisista.
+// Paluuarvona false jos häviö kriteeri täyttyy, viiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
+bool jaako_numero_yksin(vector<vector<int>>& pelilauta)
 {
     bool vasen_ylakulma = pelilauta.at(0).at(1) == 0 and pelilauta.at(1).at(0) == 0;
     bool oikea_ylakulma = pelilauta.at(0).at(3) == 0 and pelilauta.at(1).at(4) == 0;
     bool vasen_alakulma = pelilauta.at(4).at(1) == 0 and pelilauta.at(3).at(0) == 0;
     bool oikea_alakulma = pelilauta.at(4).at(3) == 0 and pelilauta.at(3).at(4) == 0;
 
-    if (vasen_ylakulma == true or oikea_ylakulma == true or vasen_alakulma == true or oikea_alakulma == true)
+    if (vasen_ylakulma == true or oikea_ylakulma == true
+            or vasen_alakulma == true or oikea_alakulma == true)
         return false;
-    for (int x = 1; x < 4; ++ x)    //eka rivi keskeltä 0,1 0,2 0,3
+
+    //Tarkistetaan onko koordinaattien 0,1;0,2;0,3 ympäristössä kaikki tyhjiä
+    for (int x = 1; x < 4; ++ x)
     {
-        if (pelilauta.at(0).at(x-1)==0 and pelilauta.at(0).at(x+1)==0 and pelilauta.at(1).at(x)==0)
+        if (pelilauta.at(0).at(x-1)==0 and pelilauta.at(0).at(x+1)==0
+                and pelilauta.at(1).at(x)==0)
             return false;
     }
 
-    for (int y = 1; y < 4; ++y)       //vasen laita
+    //Tarkistetaan onko koordinaattien 1,0;2,0;3,0 ympäristössä kaikki tyhjiä
+    for (int y = 1; y < 4; ++y)
     {
-        if (pelilauta.at(y-1).at(0)==0 and pelilauta.at(y+1).at(0)==0 and pelilauta.at(y).at(1)==0)
+        if (pelilauta.at(y-1).at(0)==0 and pelilauta.at(y+1).at(0)==0
+                and pelilauta.at(y).at(1)==0)
             return false;
     }
 
-    for (int y = 1; y < 4; ++y)       //oikea laita
+    //Tarkistetaan onko koordinaattien 1,4;2,4;3,4 ympäristössä kaikki tyhjiä
+    for (int y = 1; y < 4; ++y)
     {
-        if (pelilauta.at(y-1).at(4)==0 and pelilauta.at(y+1).at(4)==0 and pelilauta.at(y).at(3)==0)
+        if (pelilauta.at(y-1).at(4)==0 and pelilauta.at(y+1).at(4)==0
+                and pelilauta.at(y).at(3)==0)
             return false;
     }
 
-    for (int x = 1; x < 4; ++ x)    //ala laita
+    //Tarkistetaan onko koordinaattien 4,1;4,2;4,3 ympäristössä kaikki tyhjiä
+    for (int x = 1; x < 4; ++ x)
     {
-        if (pelilauta.at(4).at(x-1)==0 and pelilauta.at(4).at(x+1)==0 and pelilauta.at(3).at(x)==0)
+        if (pelilauta.at(4).at(x-1)==0 and pelilauta.at(4).at(x+1)==0
+                and pelilauta.at(3).at(x)==0)
             return false;
     }
 
+    //Viimeiseksi tarkistetaan keskellä jääneiden 9:n ruudun ympäristö
     for (int y = 1; y<4; ++y)
     {
-        for (int x = 1; x < 4; ++x)     //ylös, alas, vasen, oikea
+        for (int x = 1; x < 4; ++x)
         {
             if (pelilauta.at(y-1).at(x) == 0 and pelilauta.at(y+1).at(x) == 0
                     and pelilauta.at(y).at(x-1) == 0 and pelilauta.at(y).at(x+1) == 0)
@@ -198,7 +256,47 @@ bool jaako_numero_yksin(vector<vector<int>>& pelilauta) //tää on raskas tyyli 
     return true;
 }
 
-void kysy_poistettavat(vector<vector<int>>& pelilauta)
+// Tarkistaa esiintyykö kukin numero vain kerran pysty-ja vaakariveillä.
+// Paluuarvona true jos voiton kriteerit täyttyyviiteparametrina vector-tyypin pelilauta
+// jonka alkiot vektoreita, joiden alkion kokonaislukuja.
+bool voiton_tarkistus(vector<vector<int>>& pelilauta)
+{
+    //Tarkistetaan vaakarivit käymällä indeksit läpi for-loopissa
+    //Jokaisen luvun aiempi esiintyvyys varmistetaan tarkistus-vektoriin avulla,
+    //johon aiempien indeksien luvut on tallennettu vertailua varten
+    for (int y = 0; y < 5; ++y)
+    {
+        vector <int> tarkistus_vektori;
+        for (int x  = 0; x < 5; ++x)
+        {
+            int luku = pelilauta.at(y).at(x);
+            if (std::find(tarkistus_vektori.begin(), tarkistus_vektori.end(), luku)
+                    != tarkistus_vektori.end())
+                return false;
+            else
+                tarkistus_vektori.push_back(luku);
+        }
+    }
+
+    //Tarkistetaan pystyrivit
+    for (int x = 0; x < 5; ++x)
+    {
+        vector <int> tarkistus_vektori;
+        for (int y = 0; y < 5; ++y)
+        {
+            int luku = pelilauta.at(y).at(x);
+            if (std::find(tarkistus_vektori.begin(), tarkistus_vektori.end(), luku)
+                    != tarkistus_vektori.end())
+                return false;
+            else
+                tarkistus_vektori.push_back(luku);
+        }
+    }
+
+    return true;
+}
+
+void kysy_koordinaatteja(vector<vector<int>>& pelilauta)
 {
     while (true)
     {
@@ -207,7 +305,7 @@ void kysy_poistettavat(vector<vector<int>>& pelilauta)
 
         cout << "Enter removable element (x, y): ";
         cin >> x_mjonona;
-        if (tolower(x_mjonona.at(0)) == 'q')        //Lopetus jos 'q'
+        if (tolower(x_mjonona.at(0)) == 'q')    //Lopetus jos syötteenä 'q'
         {
             cout << "Quitting" << endl;
             return;
@@ -216,57 +314,55 @@ void kysy_poistettavat(vector<vector<int>>& pelilauta)
         int x_lukuna = stoi_with_check(x_mjonona);
         int y_lukuna = stoi_with_check(y_mjonona);
 
-
-        if (x_lukuna == false or y_lukuna == false  //Tarkistaa "out of board" kriteerit
+        //Tarkistaa onko syöte sallittu
+        if (x_lukuna == false or y_lukuna == false
                 or x_lukuna < 1 or x_lukuna > 5
                 or y_lukuna < 1 or y_lukuna > 5)
         {
             cout << "Out of board" << endl;
             continue;
         }
-        if (pelilauta.at(y_lukuna-1).at(x_lukuna-1) == 0)   //onko poistettu aiemmin
+
+        //Tarkistaa onko koordinaatit poistettu aiemmin
+        if (pelilauta.at(y_lukuna-1).at(x_lukuna-1) == 0)
         {
             cout << "Already removed" << endl;
             continue;
         }
+
+        //Asettaa koordinaatin arvoksi nollan
         pelilauta.at(y_lukuna-1).at(x_lukuna-1) = 0;
-        //tulostus
         print(pelilauta);
-        //virhetarkistukset -
-        //  -kaks nollaa vierekkäin
-        if (tyhjat_vierekkain(pelilauta) == false)
-        {
-            cout << "You lost" << endl;
-            return;                         //muuta jos muutat paluuarvontyyppiä
-        }
-        //  -numero jää yksin, huomioi reunat ja kulmat
-        if (jaako_numero_yksin(pelilauta) == false)
+
+        //Seuraavaksi häviötilanteiden tarkistukset
+        if (tyhjat_vierekkain(pelilauta) == false or jaako_numero_yksin(pelilauta) == false)
         {
             cout << "You lost" << endl;
             return;
         }
 
-        //voitontarkistus - ilmoitus ja paluu ja lopetus
-
+        //Voitontarkistus
+        if (voiton_tarkistus(pelilauta) == true)
+        {
+            cout << "You won" << endl;
+            return;
+        }
     }
 }
 
 
-//5 COMMITTIA, PITIKÖ NE PUSHATA VAI RIITTIKÖ TUO COMMIT MITKÄ TEIN
 int main()
 {
     vector<vector<int>> pelilauta;
-    string paluuarvo = kysy_tayttotapaa();
 
+    string paluuarvo = kysy_tayttotapaa();
     if (paluuarvo == "arvo")
         arvo_pelilaudan_numerot(pelilauta);
     else if (paluuarvo == "kysy")
         kysy_pelilaudan_numerot(pelilauta);
 
     print(pelilauta);
-    kysy_poistettavat(pelilauta);
-
-    cout << "Palasi mainiin poistettavista koordinaateista" << endl;
+    kysy_koordinaatteja(pelilauta);
 
 
     return 0;
