170e1b1e6fc9c063d237e5ccb86c5ea537c1be8d
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-02-17 08:02:22 +0200

    Tietorakenne ja funktiot määritelty

---
+++ student/06/network/main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
+                              "C = count id\nD = depth id\n";
+
+
+std::vector<std::string> split(const std::string& s,
+                               const char delimiter,
+                               bool ignore_empty = false)
+{
+    std::vector<std::string> result;
+    std::string tmp = s;
+
+    while(tmp.find(delimiter) != std::string::npos)
+    {
+        std::string new_part = tmp.substr(0, tmp.find(delimiter));
+        tmp = tmp.substr(tmp.find(delimiter) + 1, tmp.size());
+        if(not (ignore_empty and new_part.empty()))
+        {
+            result.push_back(new_part);
+        }
+    }
+    if(not (ignore_empty and tmp.empty()))
+    {
+        result.push_back(tmp);
+    }
+    return result;
+}
+
+using mapintietotyyppi = unordered_map <string, vector<string>> ;
+
+void store (string id1, string id2, mapintietotyyppi& mapisto)
+{
+
+}
+
+void print (string id)
+{
+
+}
+
+void count (string id)
+{
+
+}
+
+void depth (string id )
+{
+
+}
+
+int main()
+{
+    // TODO: Implement the datastructure here       //TIETORAKENNE TÄHÄN
+    unordered_map <string, vector<string>> mapisto;
+
+
+    while(true)
+    {
+        std::string line;
+        std::cout << "> ";
+        getline(std::cin, line);
+        std::vector<std::string> parts = split(line, ' ', true);        //stringrivi tulee funkusta vektoriin jaettuina takaisin
+
+        // Allowing empty inputs
+        if(parts.size() == 0)
+        {
+            continue;
+        }
+
+        std::string command = parts.at(0);
+
+        if(command == "S" or command == "s")
+        {
+            if(parts.size() != 3)
+            {
+                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+                continue;
+            }
+            std::string id1 = parts.at(1);
+            std::string id2 = parts.at(2);
+
+            // TODO: Implement the command here!        //STORE TÄHÄN 2 PARAMETRIA
+
+
+        }
+
+
+
+
+
+
+
+
+        else if(command == "P" or command == "p")
+        {
+            string pisteet = "..";
+            if(parts.size() != 2)
+            {
+                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+                continue;
+            }
+            std::string id = parts.at(1);
+
+            // TODO: Implement the command here!        //PRINT TÄHÄN
+
+
+        }
+
+
+
+
+
+
+
+
+
+        else if(command == "C" or command == "c")
+        {
+            if(parts.size() != 2)
+            {
+                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+                continue;
+            }
+            std::string id = parts.at(1);
+
+            // TODO: Implement the command here!        //COUNT TÄHÄN
+
+        }
+
+
+
+
+
+
+
+        else if(command == "D" or command == "d")
+        {
+            if(parts.size() != 2)
+            {
+                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+                continue;
+            }
+            std::string id = parts.at(1);
+
+            // TODO: Implement the command here!        //DEPTH TÄHÄN
+
+        }
+
+
+
+
+
+
+        else if(command == "Q" or command == "q")
+        {
+           return EXIT_SUCCESS;
+        }
+        else
+        {
+            std::cout << "Erroneous command!" << std::endl << HELP_TEXT;
+        }
+    }
+}
