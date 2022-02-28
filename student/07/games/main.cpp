68ac610a0cd6d8ef297e217ea43d81bb28827ed8
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-02-28 12:15:02 +0200

    Valmis ja arvosteltava versio

--- student/07/games/main.cpp
+++ student/07/games/main.cpp
@@ -16,47 +16,65 @@
  *
  *  The data file's lines should be in format game_name;player_name;score
  * Otherwise the program execution terminates instantly (but still gracefully).
- *
+
+ * Ohjelman kirjoittaja
+ * Nimi: Vesa Lukkarila
+ * Opiskelijanumero: 150372523
+ * Käyttäjätunnus: h6vlukka
+ * E-Mail: vesa.lukkarila@tuni.fi
  * */
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
-#include <fstream>          //tiedostojen lukeminen
-#include <sstream>          //stringstream
-#include <cctype>       //toupper
+#include <fstream>
+#include <sstream>
+#include <cctype>
 #include <set>
-#include <utility>      //pair
-#include <algorithm>    //sort
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
-//tietorakenne
+// tietorakenne
 using PELIEN_TIETOTYYPPI = map<string, map<string, int>>;
 
-//merkkijonovakiot
+// merkkijonovakiot
 const string VIRHE_TEKSTI = "Error: Invalid input.";
 const string EI_LOYDY_TEKSTI = " could not be found.";
 
+// Hyväksytyt komennot avaimina.
+// Hyötykuorman luku on yhtä suurempi kuin kullekin komennolle
+// vaadittu parametrimäärä.
+const map<string, unsigned int> KOMENTO_TARKISTUS = {
+                        {"ALL_GAMES", 1}, {"GAME", 2},
+                        {"ALL_PLAYERS", 1}, {"PLAYER", 2},
+                        {"ADD_GAME", 2}, {"ADD_PLAYER", 4},
+                        {"REMOVE", 2}};
+
+
 
 // Casual split func, if delim char is between "'s, ignores it.
-std::vector<std::string> split( const std::string& str, char delim = ';' )      //palauttaa vektorin jossa merkillä erotetut omissa alkioissa string:nä
+//Parametreina string ja oletuserotinmerkkinä ";"
+//Palauttaa vektorin, jonka alkiot merkkijonoja.
+std::vector<std::string> split( const std::string& str, char delim = ';' )
 {
-    std::vector<std::string> result = {""}; //palautettava vector jonka alkiot string
-    bool inside_quatation = false;  //muuttu loopissa trueksi jos '"'
+    std::vector<std::string> result = {""};
+    bool inside_quatation = false;
     for ( auto current_char : str )
     {
         if ( current_char == '"' )
         {
-            inside_quatation = not inside_quatation;        //muuttuu trueksi jos heittomerkki
+            inside_quatation = not inside_quatation;
         }
-        else if ( current_char == delim and not inside_quatation )  //jos jotain, lisää tyhjän (ei välilyöntiä) alkioksi erotinmerkin tullessa vastaan
+        else if ( current_char == delim and not inside_quatation )
         {
             result.push_back("");
         }
         else
         {
-            result.back().push_back(current_char);      //lisää merkin alkioksi, hetkinen back().pushback, työntää samaan alkioon luulen
+            result.back().push_back(current_char);
         }
     }
     if ( result.back() == "" )
@@ -66,22 +84,34 @@ std::vector<std::string> split( const std::string& str, char delim = ';' )      
     return result;
 }
 
-bool rivit_kunnossa(vector <string> const& rivivektori)
+
+
+// Tarkistaa että vektorissa on 3 alkiota ja ettei pelin (indeksi 0) tai
+// pelaajan (indeksi 1) kohta ole tyhjä.
+// Parametrina vektori, jonka alkiot merkkijonoja.
+// Palauttaa totuusarvon.
+bool onko_rivit_kunnossa(vector <string> const& rivivektori)
 {
-    //VALMIS
-    //Tarkistaa että vektorissa on 3 alkiota ja ettei pelin (indeksi 0) tai pelaajan (indeksi 1) kohta ole tyhjä
+
     return rivivektori.size() == 3
             && !rivivektori.at(0).empty()
             && !rivivektori.at(1).empty();
 }
 
+
+
+// Avaa käyttäjän syöttämän tiedoston ja tallentaa sen sisällön
+// tietorakenteeseen. Antaa virheilmoituksen mikäli sisältö
+// ei ole halutunlainen tai tiedostoa ei löydy.
+// Viiteparametrina tyhjä tietorakenne.
+// Palauttaa totuusarvon mainiin.
 bool tiedoston_avaus (PELIEN_TIETOTYYPPI& pelit_map)
 {
-    //VALMIS, voiko osia käyttää?
+
     string tiedosto = "";
     cout << "Give a name for input file: ";
     getline(cin, tiedosto);
-    ifstream tiedostonavausolio(tiedosto);      //esitellään tiedostonavausolio ja yritetään avata syötetty tiedosto
+    ifstream tiedostonavausolio(tiedosto);
 
     if (not tiedostonavausolio)
     {
@@ -93,10 +123,10 @@ bool tiedoston_avaus (PELIEN_TIETOTYYPPI& pelit_map)
     vector <string> rivi_vektori;
     while (getline(tiedostonavausolio, rivi))
     {
-
-        rivi_vektori = split(rivi);     //;-merkillä erotetu sanat omissa alkioissaan, MYÖS PISTEET STRINGINÄ!
-        //tarkistetaan että syötetiedoston rivit sisältävät halutut asiat
-        if (not rivit_kunnossa(rivi_vektori))       //jos rivit ei ole kunnossa, palautetaan false
+        rivi_vektori = split(rivi);
+        // Tarkistetaan että syötetiedoston rivit sisältävät halutut asiat
+        // apufunktion avulla
+        if (not onko_rivit_kunnossa(rivi_vektori))
         {
             cout << "Error: Invalid format in file." << endl;
             tiedostonavausolio.close();
@@ -105,19 +135,25 @@ bool tiedoston_avaus (PELIEN_TIETOTYYPPI& pelit_map)
 
         string peli = rivi_vektori.at(0);
         string pelaaja = rivi_vektori.at(1);
-        int pisteet = stoi(rivi_vektori.at(2)); //PISTEET MUUTETAAN INTIKSI
+        int pisteet = stoi(rivi_vektori.at(2));
 
-        if (pelit_map.find(peli) == pelit_map.end())    //jos peliä ei ole, lisätään avaimeksi ulompaan sanakirjaan
+        // Jos peliä ei ole, lisätään avaimeksi ulompaan sanakirjaan
+        if (pelit_map.find(peli) == pelit_map.end())
         {
             pelit_map.insert({peli, {}});
         }
-        pelit_map.at(peli).insert({pelaaja, pisteet});  //peli löytyy, lisätään sisempään sanakirjaan avain-arvo-pariksi pelaaja&pisteet
+        // Lisätään sisempään sanakirjaan avain-arvo-pariksi pelaaja&pisteet
+        pelit_map.at(peli).insert({pelaaja, pisteet});
     }
 
     return true;
 }
 
-//ALL_GAMES komento VALMIS
+
+
+// ALL_GAMES komento. Tulostaa pelit aakkostettuna.
+// Viiteparametrina tietorakenne.
+// Ei paluuarvoa.
 void pelien_tulostus(PELIEN_TIETOTYYPPI const& pelit_map)
 {
     cout << "All games in alphabetical order:" << endl;
@@ -126,23 +162,29 @@ void pelien_tulostus(PELIEN_TIETOTYYPPI const& pelit_map)
 }
 
 
-//GAME komento tulostaa pelin pisteet ja pelaajat suuruus- ja aakkosjärjestyksen mukaisesti.
-//Ei palauta tai muuta mitään.
-void pelin_tiedot (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> const& apuvektori)
+
+// GAME komento tulostaa pelin pisteet ja pelaajat suuruus- ja
+// aakkosjärjestyksen mukaisesti.
+// Parametreeina tietorakenne ja vektori jossa komennon parametrit.
+// Ei palauta tai muuta mitään.
+void pelin_tiedot (PELIEN_TIETOTYYPPI const& pelit_map,
+                   vector<string> const& apuvektori)
 {
     string pelin_nimi = apuvektori.at(1);
-    //mikäli peliä ei löydy tietorakenteesta, annetaan virheilmoitus ja poistutaan funktiosta
+    // Mikäli peliä ei löydy tietorakenteesta, annetaan virheilmoitus
+    //ja poistutaan funktiosta
     if (pelit_map.find(pelin_nimi) == pelit_map.end())
     {
         cout << "Error: Game" << EI_LOYDY_TEKSTI << endl;
         return;
     }
 
-    cout << "Game " << apuvektori.at(1) << " has these scores and players, listed in ascending order:" << endl;
+    cout << "Game " << apuvektori.at(1) <<
+            " has these scores and players, listed in ascending order:" << endl;
 
-    //map:n avain-arvo alkiot lisätään päinvastaisessa järjestyksessä apusetti:n
-    //jolloin ne ovat automaattisesti suuruusjärjestyksessä pisteiden mukaan
-    //ja aakkojärjestyksessä mikäli pisteet ovat yhtäsuuret
+    // Sisemmän sanakirjan avain&hyötykuorma lisätään apusetti:n päinvastaisessa
+    // järjestyksessä jolloin ne ovat automaattisesti suuruusjärjestyksessä
+    // pisteiden mukaan ja aakkojärjestyksessä mikäli pisteet ovat yhtäsuuret
     set <pair<int, string>> apusetti;
 
     for (auto tietopari : pelit_map.at(pelin_nimi))
@@ -151,14 +193,14 @@ void pelin_tiedot (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> const& ap
         apusetti.insert(apupair);
     }
 
-    //laskuri ensimmäisen parin käsittelemiseksi
+    // laskuri ensimmäisen parin käsittelemiseksi
     int laskuri = 0;
-    //ja pisteet_setti aiempien pisteiden vertailua varten
+    // ja pisteet_setti aiempien pisteiden vertailua varten
     set<int> pisteet_setti;
 
     for ( auto pari : apusetti)
     {
-        // Tulosta piste ja nimi jos enimmäinen alkio
+        // Tulosta piste ja nimi jos ensimmäinen alkio
         if (laskuri == 0)
         {
             cout << pari.first << " : " << pari.second;
@@ -167,7 +209,7 @@ void pelin_tiedot (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> const& ap
             continue;
         }
         // Tulosta pilkku ja nimi jos pisteet samat kuin aiemmalla
-        else if (pisteet_setti.find(pari.first) != pisteet_setti.end())        //TÄSSÄ SE VIRHE KOHTA ON TAI JOTAIN TAPAHTUU, SKIPPAA TÄMÄN
+        else if (pisteet_setti.find(pari.first) != pisteet_setti.end())
         {
             cout << ", " << pari.second;
         }
@@ -185,10 +227,13 @@ void pelin_tiedot (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> const& ap
 }
 
 
-//ALL_PLAYERS komento VALMIS
+
+// ALL_PLAYERS komento.
+// Lisätää paikallismuuttujaan (set) pelaajien nimet ja tulostetaan.
+// Parametrina tietorakenne
+// Ei palauta tai muuta mitään.
 void kaikki_pelaajat(PELIEN_TIETOTYYPPI const& pelit_map)
 {
-    //Lisätää paikallismuuttujaan pelaajien nimet ja tulostetaan.
     set <string> apusetti;
     for (auto ulompi_pari : pelit_map)
         for (auto sisempi_pari : pelit_map.at(ulompi_pari.first))
@@ -196,27 +241,32 @@ void kaikki_pelaajat(PELIEN_TIETOTYYPPI const& pelit_map)
     cout << "All players in alphabetical order: " << endl;
     for (auto alkio : apusetti)
         cout << alkio << endl;
-
 }
 
 
-//PLAYER komento kaikki pelaajan pelaamat pelit VALMIS
+
+// PLAYER komento.
+// Tulostaa kaikki pelaajan pelaamat pelit
+// Parametreina tietorakenne ja vektori jossa komennon parametrit.
+// Ei palauta tai muuta mitään.
 void pelaajan_pelit (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> apuvektori)
 {
-    //Etsitään pelaajan nimeä sisemmistä sanakirjoista ja lisätään peli apumuuttujaan pelaajan löytyessä.
-    //Jos pelaajaa ei löydy, ilmoitus ja funktiosta poistuminen
+    // Etsitään pelaajan nimeä sisemmistä sanakirjoista ja lisätään peli apumuuttujaan
+    // pelaajan löytyessä.
+    // Jos pelaajaa ei löydy, ilmoitus ja funktiosta poistuminen
     set <string> pelisetti;
     string pelaaja = apuvektori.at(1);
     for (auto ulompi_pari : pelit_map)
     {
-        if (pelit_map.at(ulompi_pari.first).find(pelaaja) != pelit_map.at(ulompi_pari.first).end())
+        if (pelit_map.at(ulompi_pari.first).find(pelaaja) !=
+                pelit_map.at(ulompi_pari.first).end())
         {
             pelisetti.insert(ulompi_pari.first);
         }
     }
     if (pelisetti.empty())
         {
-            cout << "Error: Player" << EI_LOYDY_TEKSTI << endl; //lähinnä treenin vuoksi
+            cout << "Error: Player" << EI_LOYDY_TEKSTI << endl;
             return;
         }
     cout << "Player " << apuvektori.at(1) << " playes the following games:" << endl;
@@ -225,7 +275,11 @@ void pelaajan_pelit (PELIEN_TIETOTYYPPI const& pelit_map, vector<string> apuvekt
 }
 
 
-//ADD_GAME komento. Lisää uuden pelin, jos ei ole entuudestaan.
+
+// ADD_GAME komento.
+// Lisää uuden pelin, jos ei ole entuudestaan tietorakenteessa.
+// Parametreina tietorakenne ja vektori jossa komennon parametrit.
+// Ei paluuarvoa.
 void uusi_peli (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)
 {
     string pelin_nimi = apuvektori.at(1);
@@ -239,13 +293,16 @@ void uusi_peli (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)
 }
 
 
-//ADD_PLAYER komento. Lisää pelaajan ja pisteet mikäli pelaaja uusi.
+
+// ADD_PLAYER komento. Lisää pelaajan ja pisteet mikäli pelaaja uusi.
 // Päivittää pelaajan pisteet mikäli jo olemassa.
-void uusi_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)    //MIELESTÄNI TOIMII OIKEIN, TIETOTYYPIT OK, VÄLITYS ONGELMA?
+// Parametreina tietorakenne ja vektori jossa komennon parametrit.
+// Ei paluuarvoa.
+void uusi_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)
 {
     string pelin_nimi = apuvektori.at(1);
     string pelaajan_nimi = apuvektori.at(2);
-    int pisteet = stoi(apuvektori.at(3));   //MUUTTAA PISTEET INTIKSI
+    int pisteet = stoi(apuvektori.at(3));
 
     // Peliä ei löydy, virheilmoitus ja funktiosta poistuminen.
     if (pelit_map.find(pelin_nimi) == pelit_map.end())
@@ -253,10 +310,12 @@ void uusi_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)    
         cout << "Error: Game" << EI_LOYDY_TEKSTI << endl;
         return;
     }
+
     // Peli löytyy, pelaaja on uusi - lisätään uutena
     else if (pelit_map.at(pelin_nimi).find(pelaajan_nimi)
              == pelit_map.at(pelin_nimi).end())
         pelit_map.at(pelin_nimi).insert({pelaajan_nimi, pisteet});
+
     // Jos pelaaja jo olemassa, päivitetään pisteet.
     else
         pelit_map.at(pelin_nimi).at(pelaajan_nimi) = pisteet;
@@ -267,14 +326,20 @@ void uusi_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)    
 
 }
 
-//REMOVE  Komento poistaa annetun pelaajan kaikista peleistä
+
+
+// REMOVE komento
+// Poistaa annetun pelaajan kaikista peleistä
+// Parametreina tietorakenne ja vektori jossa komennon parametrit.
+// Ei paluuarvoa.
 void poista_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)
 {
     string pelaajan_nimi = apuvektori.at(1);
     set <string> apusetti;
     for (auto pelit : pelit_map)
     {
-        if (pelit_map.at(pelit.first).find(pelaajan_nimi) != pelit_map.at(pelit.first).end())   //jos löytyy sanakirjasta, lisätään peli apusettiin
+        // Jos löytyy sanakirjasta, lisätään peli apusettiin
+        if (pelit_map.at(pelit.first).find(pelaajan_nimi) != pelit_map.at(pelit.first).end())
         {
             apusetti.insert(pelit.first);
         }
@@ -292,7 +357,9 @@ void poista_pelaaja (PELIEN_TIETOTYYPPI& pelit_map, vector<string> apuvektori)
     cout << "Player was removed from all games." << endl;
 }
 
-//KOMENTOJEN PERUSTEELLA FUNKTIOIDEN KUTSU TÄÄLTÄ
+
+
+//KOMENTOJEN perusteella funktioiden kutsu täältä.
 void funktio_kutsut (PELIEN_TIETOTYYPPI& pelit_map, string const& komento_isolla, vector<string> const& apuvektori)//pelit_map vain viitteenä, koska sitä muokataan
 {
     //Parempaa tapaa osaamatta funktiokutsut if-else rakenteella
@@ -322,21 +389,8 @@ void funktio_kutsut (PELIEN_TIETOTYYPPI& pelit_map, string const& komento_isolla
 
 
 
-
-
-//VAKIOMUUTTUJA MAP. Hyväksytyt komennot avaimina.
-//Hyötykuorman luku on yhtä suurempi kuin kullekin komennolle
-//vaadittu parametrimäärä.
-const map<string, unsigned int> KOMENTO_TARKISTUS = {{"ALL_GAMES", 1}, {"GAME", 2},//hyötykuorma muutettu sizetypeksi jotta vertailu onnistuu mainissa, aiemmin int.
-                                   {"ALL_PLAYERS", 1}, {"PLAYER", 2},   //int lukua kasvatetu yhdellä helpomman vertailun vuoksi mainissa, todellisuudessa parametreja yksi merkittyä vähemmän
-                                   {"ADD_GAME", 2}, {"ADD_PLAYER", 4},
-                                   {"REMOVE", 2}};
-
-
-
 int main()
 {
-    //tiedostorakenne
     PELIEN_TIETOTYYPPI pelit_map;
     if (!tiedoston_avaus(pelit_map))
         return 1;
@@ -359,15 +413,16 @@ int main()
             return EXIT_SUCCESS;
 
         // Tarkistetaan komennon oikeellisuus ja parametrien riittävä määrä
-        // vakiomuuttujaksi määritetyn map:n avulla.
-        //Vakiomuuttujassa hyötykuorma on yhtä suurempi kuin vaadittu
-        //parametrimäärä kullekin komennolle. Näin vertailu onnistuu suoraan.
+        // const-vakioksi määritetyn map:n avulla.
+        // KOMENTO_TARKISTUS map:ssa hyötykuorma on yhtä suurempi kuin vaadittu
+        // parametrimäärä kullekin komennolle. Näin vertailu apuvektoriin
+        // onnistuu suoraan.
         else if (KOMENTO_TARKISTUS.find(komento_isolla) != KOMENTO_TARKISTUS.end()
                 && apuvektori.size() >= KOMENTO_TARKISTUS.at(komento_isolla))
         {
-            //funktio_kutsut-funktioon välitetään kolme parametria joista vain
-            //tarpeelliset välitetään eteenpäin suorittaville funktioille
-            funktio_kutsut(pelit_map, komento_isolla, apuvektori);  //apuvektorissa pisteet stringinä
+            // funktio_kutsut-funktioon välitetään kolme parametria joista vain
+            // tarpeelliset välitetään eteenpäin suorittaville funktioille
+            funktio_kutsut(pelit_map, komento_isolla, apuvektori);
 
         }
         else
@@ -376,24 +431,7 @@ int main()
             continue;
         }
 
-
-
     }
-    //TEKEMÄSSÄ:
-
-
-
-
     return EXIT_SUCCESS;
 }
 
-//1 commit tiedoston luku ja ilmoistukset
-//2 COMMIT mainissa komennon ja parametrien tarkistus ja funktio_kutsut valmis
-
-
-//omia testejä...
-//for (auto tietopari : pelit_map)
-//{
- //   cout << tietopari.first << endl;
-  //  for(auto sisempipari :  pelit_map.at(tietopari.first))
-    //    cout << sisempipari.first << endl;
