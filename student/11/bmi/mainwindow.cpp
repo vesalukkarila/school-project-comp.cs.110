97bc302c3cc231087f8b94aebb7c7f49401ea4a7
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-03-31 14:23:21 +0300

    Toimii

---
+++ student/11/bmi/mainwindow.cpp
@@ -0,0 +1,58 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+#include<algorithm>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_weightLineEdit_editingFinished()
+{
+    paino_string = ui->weightLineEdit->text();
+    paino = paino_string.toDouble();
+}
+
+
+void MainWindow::on_heightLineEdit_editingFinished()
+{
+    pituus_string = ui->heightLineEdit->text();
+    pituus = pituus_string.toDouble();
+    pituus /= 100;
+
+}
+
+
+void MainWindow::on_countButton_clicked()
+{
+    if (paino == 0 or paino_string.length() == 0)
+    {
+        ui->resultLabel->setText("Cannot count");
+
+    }
+    else
+    {
+        indeksi = (paino/(pow(pituus, 2)));
+        indeksi_string.setNum(indeksi);
+        ui->resultLabel->setText(indeksi_string);
+        if(indeksi < 18.5)
+            ui->infoTextBrowser->setText("You are underweight");
+        else if (indeksi > 25)
+            ui->infoTextBrowser->setText("You are overweight");
+        else
+            ui->infoTextBrowser->setText("You weight is normal");
+
+
+    }
+
+}
+
---
+++ student/11/bmi/mainwindow.hh
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_weightLineEdit_editingFinished();
+
+    void on_heightLineEdit_editingFinished();
+
+    void on_countButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString paino_string,
+            pituus_string,
+            indeksi_string;
+    double pituus,
+        paino,
+        indeksi;
+
+};
+#endif // MAINWINDOW_HH
