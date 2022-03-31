821118b020ce57d8e1e6f6c1273ca93919da97e3
Vesa Lukkarila <vesa.lukkarila@tuni.fi>
2022-03-31 12:33:05 +0300

    Ei pyöri, harjoitustehtävän mukaan tehty

--- student/11/timer/mainwindow.cpp
+++ student/11/timer/mainwindow.cpp
@@ -6,9 +6,45 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    timer = new QTimer(parent); //jäi epäselväksi, luodaan osoittimen päähän olio kirjastosta vissiin
+    connect(timer, &QTimer::timeout, this, &MainWindow::updateLcd); //yhdistetään olioita ymmärtäisin
+
+    ui->lcdNumberMin->setStyleSheet("background-color: darkblue ");
+    ui->lcdNumberSec->setStyleSheet("background-color: orange ");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_startButton_clicked()
+{
+    if (ui->lcdNumberMin == 0 and ui->lcdNumberSec == 0)
+    {
+        timer->start(1000);
+        time = 0;
+    }
+    }
+
+
+void MainWindow::on_stopButton_clicked()
+{
+    timer->stop();
+}
+
+
+void MainWindow::on_resetButton_clicked()
+{
+    ui->lcdNumberMin->display(0);
+    ui->lcdNumberSec->display(0);
+    on_startButton_clicked();
+}
+
+void MainWindow::updateLcd()
+{
+    time++;
+    ui->lcdNumberMin->display(time/60);
+    ui->lcdNumberSec->display(time&60);
+}
+
--- student/11/timer/mainwindow.hh
+++ student/11/timer/mainwindow.hh
@@ -24,8 +24,16 @@ public:
 private slots:
     // Add your slots here
 
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_resetButton_clicked();
+    void updateLcd();
+
 private:
     Ui::MainWindow *ui;
+    int time;
     // Add other necessary members here
 };
 
